74d1ed0cb13df51756409c0e7efa53781a7cd2e5
Administrator <Administrator@A2010115>
2016-09-17 09:11:52 +0800

    添加项目文件。

---
+++ 淘宝众筹/Form1.Designer.vb
@@ -0,0 +1,120 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form 重写 Dispose，以清理组件列表。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows 窗体设计器所必需的
+    Private components As System.ComponentModel.IContainer
+
+    '注意: 以下过程是 Windows 窗体设计器所必需的
+    '可以使用 Windows 窗体设计器修改它。  
+    '不要使用代码编辑器修改它。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.WebBrowser2 = New System.Windows.Forms.WebBrowser()
+        Me.WebBrowser1 = New System.Windows.Forms.WebBrowser()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.RichTextBox1 = New System.Windows.Forms.RichTextBox()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'WebBrowser2
+        '
+        Me.WebBrowser2.Location = New System.Drawing.Point(-12, 64)
+        Me.WebBrowser2.MinimumSize = New System.Drawing.Size(20, 20)
+        Me.WebBrowser2.Name = "WebBrowser2"
+        Me.WebBrowser2.ScriptErrorsSuppressed = True
+        Me.WebBrowser2.Size = New System.Drawing.Size(758, 420)
+        Me.WebBrowser2.TabIndex = 10
+        Me.WebBrowser2.Url = New System.Uri("http://wan.meizu.com/", System.UriKind.Absolute)
+        '
+        'WebBrowser1
+        '
+        Me.WebBrowser1.Location = New System.Drawing.Point(-12, 490)
+        Me.WebBrowser1.MinimumSize = New System.Drawing.Size(20, 20)
+        Me.WebBrowser1.Name = "WebBrowser1"
+        Me.WebBrowser1.ScriptErrorsSuppressed = True
+        Me.WebBrowser1.Size = New System.Drawing.Size(1270, 266)
+        Me.WebBrowser1.TabIndex = 9
+        Me.WebBrowser1.Url = New System.Uri("https://izhongchou.taobao.com/dreamdetail.htm?spm=a215p.1472805.0.0.o2RZBb&id=200" &
+        "46906", System.UriKind.Absolute)
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(590, 35)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 8
+        Me.Button1.Text = "开始填表"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(-12, 37)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(596, 21)
+        Me.TextBox1.TabIndex = 7
+        Me.TextBox1.Text = "https://izhongchou.taobao.com/dreamdetail.htm?spm=a215p.1472805.0.0.4R0EaR&id=200" &
+    "47057"
+        '
+        'Timer1
+        '
+        Me.Timer1.Interval = 1500
+        '
+        'RichTextBox1
+        '
+        Me.RichTextBox1.Location = New System.Drawing.Point(752, 39)
+        Me.RichTextBox1.Name = "RichTextBox1"
+        Me.RichTextBox1.Size = New System.Drawing.Size(496, 445)
+        Me.RichTextBox1.TabIndex = 12
+        Me.RichTextBox1.Text = ""
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(671, 37)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 11
+        Me.Button2.Text = "登录"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1263, 759)
+        Me.Controls.Add(Me.WebBrowser2)
+        Me.Controls.Add(Me.WebBrowser1)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.RichTextBox1)
+        Me.Controls.Add(Me.Button2)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents WebBrowser2 As WebBrowser
+    Friend WithEvents WebBrowser1 As WebBrowser
+    Friend WithEvents Button1 As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents RichTextBox1 As RichTextBox
+    Friend WithEvents Button2 As Button
+End Class
---
+++ 淘宝众筹/Form1.vb
@@ -0,0 +1,139 @@
+﻿
+Imports System.Net
+Imports HtmlAgilityPack
+Imports System.Text
+Public Class Form1
+
+    Dim requireURL As String = "https://izhongchou.taobao.com/dreamdetail.htm?spm=a215p.1472805.0.0.o2RZBb&id=20046906"
+
+    Structure TaobaoZC
+        Dim allHTML
+        Dim Name
+        Dim Slogan
+        Dim Company
+        Dim URL
+        Dim CoverURL
+        Dim Price
+    End Structure
+
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        WebBrowser1.Navigate(TextBox1.Text)
+    End Sub
+
+    Private Sub WebBrowser1_DocumentCompleted(sender As Object, e As WebBrowserDocumentCompletedEventArgs) Handles WebBrowser1.DocumentCompleted
+        If Not e.Url.ToString = WebBrowser1.Url.ToString Then
+            Exit Sub
+        ElseIf WebBrowser1.ReadyState = WebBrowserReadyState.Complete Then
+            Timer1.Enabled = True
+        End If
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        Timer1.Enabled = False
+        Dim hiTaobao As New TaobaoZC
+
+        Dim EncodingOfEXP As Encoding = Encoding.GetEncoding(WebBrowser1.Document.Encoding)
+        Dim htmlMessage As String = WebBrowser1.Document.Body.InnerHtml
+        hiTaobao.allHTML = Replace(htmlMessage, "¥", "ALITAOBAOZHONGCHOU")
+        hiTaobao.URL = WebBrowser1.Url.ToString
+        hiTaobao.Name = TextProcessing.CutString(hiTaobao.allHTML, "<DIV class=project-title>
+<H1>", "</H1></DIV>")
+        hiTaobao.Slogan = TextProcessing.CutString(hiTaobao.allHTML, "项目介绍</H2>
+<DIV class=box>
+<DIV>", "</DIV>")
+        hiTaobao.Company = TextProcessing.CutString(hiTaobao.allHTML, "class=sponsor-info><SPAN class=""ww-light ww-large"" data-nick=""", """ data-display")
+        hiTaobao.CoverURL = TextProcessing.CutString(hiTaobao.allHTML, "class=cover><IMG class=cover-img src=""", """>")
+        hiTaobao.Price = TextProcessing.CutString(hiTaobao.allHTML, "repay-money><SPAN class=yen>ALITAOBAOZHONGCHOU</SPAN>", ".")
+        Try
+            WebBrowser2.Document.GetElementById("product-share-url").Focus()
+            WebBrowser2.Document.GetElementById("product-share-url").InnerText = hiTaobao.URL
+            WebBrowser2.Document.GetElementById("product-share-title").Focus()
+            WebBrowser2.Document.GetElementById("product-share-title").InnerText = hiTaobao.Name
+            For Each it As HtmlElement In WebBrowser2.Document.All.GetElementsByName("description")
+                it.Focus()
+                it.InnerText = hiTaobao.Slogan
+            Next
+            WebBrowser2.Document.GetElementById("product-share-save").Focus()
+            WebBrowser2.Document.GetElementById("product-share-save").InvokeMember("click")
+
+
+
+        Catch ex As Exception
+            Debug.WriteLine(ex.Message)
+        End Try
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        If MessageBox.Show("确认已经登录？", "确认已经登录？", MessageBoxButtons.YesNo) = DialogResult.Yes Then
+            Button1.Enabled = True
+            WebBrowser2.Navigate("http://wan.meizu.com/product/share")
+        End If
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        WebBrowser2.Navigate("http://wan.meizu.com/product/share")
+        MessageBox.Show(My.Computer.Registry.GetValue("HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Internet Explorer\MAIN\FeatureControl\FEATURE_BROWSER_EMULATION", IO.Path.GetFileName(Application.ExecutablePath), 120))
+        If Not My.Computer.Registry.GetValue("HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Internet Explorer\MAIN\FeatureControl\FEATURE_BROWSER_EMULATION", IO.Path.GetFileName(Application.ExecutablePath), 120) = 9999 Then
+            My.Computer.Registry.SetValue("HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Internet Explorer\MAIN\FeatureControl\FEATURE_BROWSER_EMULATION", IO.Path.GetFileName(Application.ExecutablePath), 9999)
+            MessageBox.Show("位置信息已更新，请重新打开应用程序！")
+            Me.Close()
+            Me.Dispose()
+
+            End
+        End If
+        Debug.WriteLine(IO.Path.GetFileName(Application.ExecutablePath))
+    End Sub
+End Class
+
+Class TextProcessing
+    Shared Function CutString(ResourceString As String, Head As String, Tail As String)
+        Try
+            Dim cutStart = InStr(ResourceString, Head) + Head.Length
+            Dim cutLength = InStr(Start:=cutStart, String1:=ResourceString, String2:=Tail) - InStr(ResourceString, Head) - Head.Length
+            Return (Mid(ResourceString, cutStart, cutLength))
+        Catch ex As Exception
+
+        End Try
+
+    End Function
+End Class
+
+Class Net
+    Shared Function GetHTMLDoc(URL As String)
+        Dim rq As HttpWebRequest
+        Try
+            If InStr(URL, "http://") Or InStr(URL, "https://") Then
+                rq = WebRequest.Create(URL)
+            Else
+                rq = WebRequest.Create("http://" & URL)
+            End If
+        Catch ex As Exception
+            Return False
+            Exit Function
+        End Try
+        Dim rp As HttpWebResponse = rq.GetResponse()
+        Dim reader As IO.StreamReader = New IO.StreamReader(rp.GetResponseStream(), System.Text.Encoding.GetEncoding("gb2312"))
+        Dim resourceString = reader.ReadToEnd
+        Return resourceString
+    End Function
+
+
+    Shared Function DownloadWebImage(ByVal ImageURL As String)
+        Dim objImage As IO.MemoryStream
+        Dim objwebClient As WebClient
+        Dim sURL As String = Trim(ImageURL)
+        Dim bAns As Boolean
+        Try
+            If Not sURL.ToLower().StartsWith("http://") Or Not sURL.ToLower().StartsWith("https://") Then sURL = "http://" & sURL
+            objwebClient = New WebClient()
+            objImage = New IO.MemoryStream(objwebClient.DownloadData(sURL))
+            Return Image.FromStream(objImage)
+        Catch ex As Exception
+            Return False
+        End Try
+        Return bAns
+    End Function
+End Class
